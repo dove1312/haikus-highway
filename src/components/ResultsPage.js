189fd0db53d9b60603ac4f798dcbe34e75d359c3
Alix <alixwagner@yahoo.ca>
2022-03-20 16:24:26 -0400

    some basic data movement between word generator components established

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,13 +1,21 @@
 //ResultsPage.js
+
+import { useEffect, useState } from "react";
 import DisplayHaiku from "./DisplayHaiku";
 import WordGenerator from "./WordGenerator";
+import wordListApiCall from "./wordListApiCall";
 
 const ResultsPage=()=> {
+     const [firstWordList, setFirstWordList] = useState([]);
+
+     // const { userWord } = useParams();
+     const userWord = "cat"
+
      return(
           <section className="resultsPage">
                <h2>Here are your results</h2>
                <DisplayHaiku />
-               <WordGenerator />
+               <WordGenerator initialWord={ userWord } />
           </section>
      )
 }
--- src/components/WordGenerator.js
+++ src/components/WordGenerator.js
@@ -1,6 +1,27 @@
-const WordGenerator = () => {
+import WordList from "./WordList";
+
+const WordGenerator = (props) => {
+    // temporary until State is added:
+    let currentHaiku = [["cat"]];
+
+    
+    // to be replaced with a real function:
+    const someSyllableFunction = (syllableInfo) => {
+        console.log(syllableInfo);
+        // probably State Stuff
+        // ex) setCurrentSyllables(currentSyllables + syllableInfo)
+    }
+    // to be replaced with a real function:
+    const someHaikuArrayFunction = (nextWord) => {
+        console.log(nextWord);
+        // probably State Stuff
+    }
+
     return (
-        <h2>words</h2>
+        <div className="wordBox">
+            <h2>words</h2>
+            <WordList initialWord={ props.initialWord } handleSyllables={ someSyllableFunction } handleHaikuWords={ someHaikuArrayFunction } />
+        </div>
     )
 }
 
