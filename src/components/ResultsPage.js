c0e23edc0d6846f245730ae7a481de5cfc27deec
Alix <alixwagner@yahoo.ca>
2022-03-21 16:04:00 -0400

    imported UseParams to ResultsPage

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,18 +1,16 @@
 //ResultsPage.js
-
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import WordGenerator from "./WordGenerator";
 
 const ResultsPage=()=> {
      const { userWord } = useParams();
-     // const userWord = "cat"
-
-     const [currentHaiku, setCurrentHaiku] = useState([]);
+     console.log(userWord)
 
      return(
           <section className="resultsPage">
                <h2>Here are your results</h2>
-               <WordGenerator currentHaiku={currentHaiku} updateHaiku={ setCurrentHaiku } initialWord={ userWord } />
+               <WordGenerator initialWord={ userWord } />
           </section>
      )
 }
