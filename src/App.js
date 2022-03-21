056428deb3680f11908600a78148948f55822f09
EmilyRead88 <read.emily88@gmail.com>
2022-03-21 15:49:38 -0400

    reconfigure routes with param

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import {Link, Route, Routes} from 'react-router-dom';
 //may also need to add in Outlet as we progress
 
-import ResultsPage from './components/ResultsPage';
-import SavedHaikus from "./components/SavedHaikus";
+import ResultsPage from './ResultsPage';
+import SavedHaikus from './SavedHaikus';
 
 function App() {
   return (
     <div>
-      {/* insert nav here */}
       <h1>hello friends!</h1>
 
       <Routes>
-        <Route path ="/" element= { <Home />}/>
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="/haikus" element={<SavedHaikus />} />
+        <Route path="/" element={<Home />}/>
+        <Route path="/results/:userWord" element={<ResultsPage />} />
+        <Route path="/haiku" element= {<SavedHaikus />}/>
       </Routes>
+
     </div>
   );
 }
@@ -23,7 +23,7 @@ function App() {
 export default App;
 
 
-//I laid it like this because home page is a component off of App.js, so path should be /home
+//I laid it like this because home page is a component off of App.js, so path should be /home 
   //we should be able to go back to the initial page to start again, so nested results and saved Haikus inside home
   //IF YOU ALL THINK THIS IS CORRECT- then we need to have a <Link> for the home component to send users to the results page once word is selected and submitted
 
