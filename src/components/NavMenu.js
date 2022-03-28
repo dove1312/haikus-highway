6ec94b9edd95054f43c2fb378d765b3cd5aef58d
Alix <alixwagner@yahoo.ca>
2022-03-28 10:19:57 -0400

    mobile menu added

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,21 +1,54 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 const NavMenu = () => {
+
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const onClick = () => {
+        setOpenMenu(!openMenu)
+    }
     return(
-        <nav>
-            <div className="wrapper">
-                <Link className='homeButton' to="/">
-                    <h1>Haikus Highway</h1>
-                </Link>
-                <ul>
-                    <li className='haikuGenerator'>
-                        <Link className='navButton' to="/haiku">Write A Haiku</Link>
-                    </li>
-                    <li>
-                        <Link className='navButton primary' to="/savedHaikus">Saved Haikus</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <>
+            <nav className="desktopNav">
+                <div className="wrapper">
+                    <Link className='homeButton' to="/">
+                        <h1>Haikus Highway</h1>
+                    </Link>
+                    <ul>
+                        <li className='haikuGenerator'>
+                            <Link className='navButton' to="/haiku">Write A Haiku</Link>
+                        </li>
+                        <li>
+                            <Link className='navButton' to="/savedHaikus">Saved Haikus</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <nav className="mobileNav">
+                <div className="wrapper">
+                    <Link className='homeButton' to="/">
+                        <h1>Haikus Highway</h1>
+                    </Link>
+                    <button onClick={ onClick } aria-label="show or hide navigation menu" className="menuIcon">
+                        <div className={`hamburgerMenuIcon ${ openMenu ? " open" : null }`}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </button>
+                    <ul className={`mainMenu ${openMenu ? "open" : null}`}>
+                        <li className='haikuGenerator'>
+                            <Link className='navLink' to="/haiku">Write A Haiku</Link>
+                        </li>
+                        <li>
+                            <Link className='navLink' to="/savedHaikus">Saved Haikus</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
     )
 }
 
