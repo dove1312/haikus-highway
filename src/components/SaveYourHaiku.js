b7c3a0ea52adcf26c46f8b9ff0c1a420d787642b
EmilyRead88 <read.emily88@gmail.com>
2022-03-29 11:35:48 -0400

    add state for save button for haiku

--- src/components/SaveYourHaiku.js
+++ src/components/SaveYourHaiku.js
@@ -3,12 +3,15 @@ import { getDatabase, ref, push } from 'firebase/database';
 import { useState } from 'react';
 
 const SaveYourHaiku = (props)=>{
+
+     const [saveButton, setSaveButton] = useState("Save Your Haiku");
      const database = getDatabase(firebase);
      const dbRef = ref(database);
      
 
      const handleClick = ()=> {
           const firebaseHaiku = push(dbRef, props.currentHaiku);
+          setSaveButton("Saved");
      }
 
 
@@ -16,7 +19,7 @@ const SaveYourHaiku = (props)=>{
      return(
           <div className="buttonContainer">
                <button
-               onClick= {handleClick}>Save Your Haiku</button>
+               onClick= {handleClick}>{saveButton}</button>
           </div>
      )     
 }
